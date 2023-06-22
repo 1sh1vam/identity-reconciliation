122f58580d6eaa1d9105b1cd7c63bfec54028889
1sh1vam <shivam07.mt@gmail.com>
2023-06-22 22:44:15 +0530

    setting up the db and env variables

---
+++ src/db.ts
@@ -0,0 +1,26 @@
+import mysql from 'mysql2/promise';
+
+class Database {
+  private _client?: mysql.Connection;
+
+  get client() {
+    if (!this._client) {
+      throw new Error('Can not access client before connecting.');
+    }
+
+    return this._client;
+  }
+
+  async connect(password: string) {
+    const client = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      database: 'bite_speed',
+      password
+    });
+
+    this._client = client;
+  }
+}
+
+export const db = new Database();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import 'dotenv/config';
 import { app } from "./app";
+import { db } from "./db";
 
 const start = async () => {
+    try {
+        // replace this password with db password
+        await db.connect(process.env.DB_PASSWORD!);
+    } catch(err) {
+        console.log('failed to connect to db', err);
+    }
     app.listen(3000, () => {
         console.log('Server started on port 3000');
     });
