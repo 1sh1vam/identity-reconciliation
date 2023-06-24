b7d6ee9ebc78566ad75709c04c581d190c88c619
1sh1vam <shivam07.mt@gmail.com>
2023-06-24 22:35:10 +0530

    create the table if table does not exists

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const start = async () => {
     try {
         // replace this password with db password
         await db.connect(process.env.DB_PASSWORD!);
+        await db.client.query('CREATE TABLE IF NOT EXISTS bite_speed.contacts (id INT PRIMARY KEY AUTO_INCREMENT, phoneNumber INT, email VARCHAR(50), linkedId INT, linkPrecedence CHAR(20), createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, deletedAt BOOLEAN)')
     } catch(err) {
         console.log('failed to connect to db', err);
     }
